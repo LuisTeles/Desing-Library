cc0d689497c11bdd09b6d500961fd92470b4e98f
LuisTeles <devzluis@gmail.com>
2025-06-19 20:09:20 -0300

    glass components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Glass from "@/components/sections/Glass";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 justify-center items-center h-screen font-[family-name:var(--font-geist-sans)] bg-linear-45 from-blue-900 via-gray-800 to-gray-800 ">
+    <div className="flex flex-1 justify-center items-center h-screen font-[family-name:var(--font-geist-sans)] bg-linear-to-br bg-violet-600/20">
       <Glass />
+      <div className="absolute top-10 left-10 w-[70vw] h-[60vh] bg-white/20 z-0 hover:z-50 rounded-lg border-1 border-white/50 backdrop-blur-lg"/>
     </div>
   );
 }
--- src/components/sections/Glass.tsx
+++ src/components/sections/Glass.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Glass = () => {
   return (
-    <div className='flex flex-col items-start justify-start p-10 bg-white/5 w-[70vw] h-[60vh] rounded-lg  border border-white/50 z-50 backdrop-blur-xl'>
+    <div className='flex flex-col items-start justify-start p-10 bg-white/5 w-[70vw] h-[60vh] rounded-lg  border border-white/50 z-50 backdrop-blur-xl drop-shadow-lg drop-shadow-black/20'>
         <h1 className='text-white text-3xl font-semibold'>HELLO</h1>
-        <p className='bg-black/50 rounded-lg p-6 mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate illo voluptate odit similique, nostrum iste alias earum quos libero! Eius, doloribus! Hic molestias possimus aut necessitatibus provident? Asperiores, natus pariatur?</p>
-        <div className='flex flex-1 w-full mt-10 p-6 rounded-lg bg-black/50'>
-            Hi
+        <p className='bg-white/10 rounded-lg p-6 mt-10 ring-1 ring-white/50 text-black/70'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate illo voluptate odit similique, nostrum iste alias earum quos libero! Eius, doloribus! Hic molestias possimus aut necessitatibus provident? Asperiores, natus pariatur?</p>
+        <div className='flex flex-1 flex-row w-full mt-10 p-6 rounded-lg gap-6 justify-center items-end'>
+            <div className='h-full w-14 bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-lg'></div>
+            <div className='h-[40%] w-14 bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-lg'></div>
+            <div className='h-[60%] w-14 bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-lg'></div>
+            <div className='h-[90%] w-14 bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-lg'></div>
+            <div className='h-[80%] w-14 bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-lg'></div>
         </div>
     </div>
   )
