005ebeab01f176ca52bd06760d8ec2f2aecf30bb
LuisTeles <devzluis@gmail.com>
2025-06-19 19:49:14 -0300

    Glass experiment

---
+++ src/components/sections/Glass.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Glass = () => {
+  return (
+    <div className='flex flex-col items-start justify-start p-10 bg-white/5 w-[70vw] h-[60vh] rounded-lg  border border-white/50 z-50 backdrop-blur-xl'>
+        <h1 className='text-white text-3xl font-semibold'>HELLO</h1>
+        <p className='bg-black/50 rounded-lg p-6 mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate illo voluptate odit similique, nostrum iste alias earum quos libero! Eius, doloribus! Hic molestias possimus aut necessitatibus provident? Asperiores, natus pariatur?</p>
+        <div className='flex flex-1 w-full mt-10 p-6 rounded-lg bg-black/50'>
+            Hi
+        </div>
+    </div>
+  )
+}
+
+export default Glass
